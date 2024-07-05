82881499f1f4387f2ba5a801dadd08778dcbb794
Wojtek08 <woj-maz1401@o2.pl>
2024-07-05 17:29:49 +0200

    05.07.2024

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let loading = document.getElementById("loading");
 let menuBtn = document.getElementById("menu-btn");
 let menu = document.getElementById("navbar");
 let main2 = document.getElementById("main2");
+let main3 = document.getElementById("main3");
 let box = document.getElementById("box");
 
 menuBtn.addEventListener("click", function() {
@@ -25,7 +26,6 @@ box.addEventListener("click", function() {
     }
 })
 
-// po kliknieciu w menuBtn musi sie zmieniac justify content i display i do body1 klasa open
 
 function mainSite() {
     let mainContent = document.getElementById("main");
@@ -38,6 +38,7 @@ function mainSite() {
     document.querySelector(".body1").classList.add("open");
     menu.classList.remove("closed")
     main2.classList.add("open")
+    main3.classList.add("open")
 }
 
 nextBtn.addEventListener("click", function() {
@@ -59,3 +60,31 @@ window.addEventListener("load", function() {
     }, 500);
 })
 
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((el) => {
+        console.log("entry");
+        if(el.isIntersecting) {
+            el.target.classList.add("seen")
+        } else {
+            el.target.classList.remove("seen")
+        }
+    })
+})
+
+const hiddenELements = document.querySelectorAll(".hidden");
+const hiddenELementsL = document.querySelectorAll(".hiddenL");
+hiddenELements.forEach((el) => observer.observe(el));
+hiddenELementsL.forEach((el) => observer.observe(el));
+
+document.addEventListener('scroll', function() {
+    var scrollTop = window.scrollY;
+    if (scrollTop > 80) {
+        document.querySelector("header").classList.add("scrolled");
+        document.querySelector(".both").classList.add("scrolled");
+    } else {
+        document.querySelector("header").classList.remove("scrolled");
+        document.querySelector(".both").classList.remove("scrolled");
+    }
+
+    console.log('Scroll Percent:', scrollTop);
+});
