b8fa26c92fa7ba9ab716b69cc7818108ea2bc0f2
Wojtek08 <woj-maz1401@o2.pl>
2024-07-06 14:37:09 +0200

    06.07.2024

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let menu = document.getElementById("navbar");
 let main2 = document.getElementById("main2");
 let main3 = document.getElementById("main3");
 let box = document.getElementById("box");
+let planetsGallery = document.getElementById("planets-gallery")
 
 menuBtn.addEventListener("click", function() {
     if(menuBtn.className == "norm open") {
@@ -77,7 +78,7 @@ hiddenELements.forEach((el) => observer.observe(el));
 hiddenELementsL.forEach((el) => observer.observe(el));
 
 document.addEventListener('scroll', function() {
-    var scrollTop = window.scrollY;
+    let scrollTop = window.scrollY;
     if (scrollTop > 80) {
         document.querySelector("header").classList.add("scrolled");
         document.querySelector(".both").classList.add("scrolled");
@@ -85,6 +86,52 @@ document.addEventListener('scroll', function() {
         document.querySelector("header").classList.remove("scrolled");
         document.querySelector(".both").classList.remove("scrolled");
     }
-
-    console.log('Scroll Percent:', scrollTop);
+    
+    if (scrollTop > 150) {
+        document.querySelector("#home").classList.add("scrolled");
+    } else {
+        document.querySelector("#home").classList.remove("scrolled");
+    }
 });
+
+function scrollPlanets() {
+    let chevron1 = document.querySelector("#planets-cont section i:last-of-type");
+    let chevron2 = document.querySelector("#planets-cont section i:first-of-type");
+    let planetsGal = document.querySelector("#planets-gallery");
+    
+
+    let curImg = 1
+
+    chevron1.addEventListener("click", function() {
+        let img1 = document.querySelector()
+        switch(curImg) {
+            case 1:
+                chevron1.style.setProperty("opacity", "0.3");
+                console.log(curImg)
+                curImg = 0;
+                break;
+            case 2:
+                chevron2.style.setProperty("opacity", "1");
+                curImg = 1;
+                console.log(curImg)
+                break;                
+        }
+    })
+
+    chevron2.addEventListener("click", function() {
+        switch(curImg) {
+            case 0:
+                chevron1.style.setProperty("opacity", "1");
+                console.log(curImg)
+                curImg = 1;
+                break;
+            case 1:
+                chevron2.style.setProperty("opacity", "0.3");
+                curImg = 2;
+                console.log(curImg)
+                break;
+        }
+    })
+}
+
+scrollPlanets()
